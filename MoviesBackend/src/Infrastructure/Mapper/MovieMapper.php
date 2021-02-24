c7a7a874f5701fe979a13578b9e58824f5bc2075
Alejandro Aranda <alexarnd99@gmail.com>
2021-02-24 19:29:40 +0100

    Refactor movieMapper

--- MoviesBackend/src/Infrastructure/Mapper/MovieMapper.php
+++ MoviesBackend/src/Infrastructure/Mapper/MovieMapper.php
@@ -4,21 +4,39 @@ namespace App\Infrastructure\Mapper;
 
 use App\Domain\Model\MoviesModel;
 use App\Infrastructure\Entity\Movies;
+use App\Infrastructure\Mapper\GenreMapper;
+use App\Application\Repository\GenreRepository;
 
 class MovieMapper extends AbstractDataMapper
 {
+    private GenreRepository $genreRepository;
+    private GenreMapper $genreMapper;
+
+    public function __construct(GenreRepository $genreRepository, GenreMapper $genreMapper)
+    {
+        $this->genreRepository = $genreRepository;
+        $this->genreMapper = $genreMapper;
+    }
+
     public function toEntity(MoviesModel $movieModel): ?Movies
     {
-        return new Movies(
+        $genres = $movieModel->getGenres();
+        $genresEntities = $this->toArrayEntityGenre($genres);
+        $movie = new Movies(
             $movieModel->getTitle(), 
             $movieModel->getOverview(), 
             $movieModel->getReleaseDate(), 
             $movieModel->getDuration(), 
         );
+
+        foreach($genresEntities as $genre){
+            $movie->addGenre($genre);
+        }
+        return $movie;
     }
     public function toModel(Movies $movieEntity): ?MoviesModel
     {
-        return new MoviesModel(
+        $movieModel = new MoviesModel(
             $movieEntity->getTitle(), 
             $movieEntity->getOverview(), 
             $movieEntity->getReleaseDate(), 
@@ -26,5 +44,24 @@ class MovieMapper extends AbstractDataMapper
             $movieEntity->getId(),
             $movieEntity->getIsDeleted() 
         );
+
+        $genreModels = $this->genreMapper->toArrayModel($movieEntity->getGenres()->toArray());
+        
+        foreach($genreModels as $genre){
+            $movieModel->addGenre($genre);
+        }
+
+        return $movieModel;
+    }
+
+    private function toArrayEntityGenre(array $models): array
+    {
+        $entities = [];
+
+        foreach($models as $model){
+            $entities[] = $this->genreRepository->findOneByName($model->getName(), true);
+        }
+        
+        return $entities;
     }
 }
