fc67720357c49ec957ae882550975f07ef5c00e4
Alejandro Aranda <alexarnd99@gmail.com>
2021-03-02 13:54:45 +0100

    Add lexik bundle to authenticate user with jwt

--- MoviesBackend/startSymfony.sh
+++ MoviesBackend/startSymfony.sh
@@ -6,4 +6,12 @@ composer install
 
 php bin/console doctrine:migrations:migrate --no-interaction
 
+if [ ! -d "config/jwt" ];
+    then mkdir "config/jwt"
+    chmod -R 777 config/jwt
+    openssl genrsa -aes128 -out config/jwt/private.pem -passout pass:password 3072
+    openssl pkey  -in auth/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:password
+    chmod -R 777 config/jwt/*.pem
+fi
+
 apachectl -D FOREGROUND
