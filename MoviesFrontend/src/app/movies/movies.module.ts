a96fd796c6864173d8880abdcc3d3b4068da258c
Alejandro Aranda <alexarnd99@gmail.com>
2021-04-02 22:06:35 +0200

    Create advanced search component

--- MoviesFrontend/src/app/movies/movies.module.ts
+++ MoviesFrontend/src/app/movies/movies.module.ts
@@ -3,8 +3,9 @@ import { SharedModule } from '../shared/shared.module';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesSearchComponent } from './pages/movies-search/movies-search.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { AdvancedSearchComponent } from './pages/movies-search/advanced-search/advanced-search.component'
 
-const COMPONENTS = [MoviesSearchComponent]
+const COMPONENTS = [MoviesSearchComponent, AdvancedSearchComponent]
 
 @NgModule({
   declarations: COMPONENTS,
