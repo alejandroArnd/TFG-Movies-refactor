bb56e9eb6215521ce48f6f7fac170bea31e2294a
Alejandro Aranda <alexarnd99@gmail.com>
2021-04-13 00:02:01 +0200

    Add validators before send user to create

--- MoviesFrontend/src/app/user/pages/register/register.component.ts
+++ MoviesFrontend/src/app/user/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/shared/service';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +10,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   public registerForm: FormGroup;
+  public submitted: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
       email:['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
-      password:['', [Validators.required, Validators.minLength(8)]]
-  });
+      password:['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword:['', Validators.required]
+  }, { validators: this.passwordsMatch});
   }
+
+  private passwordsMatch(group: FormGroup) {
+    const password = group.controls.password.value;
+    const confirmPassword = group.controls.confirmPassword.value;
+
+    return password === confirmPassword ? null : { notSame: true };
+}
   
   onSubmit(): void {
-    console.log(this.registerForm.value)
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.userService.createUser(this.registerForm.value).subscribe(    
+      data => {
+     })
   }
+
+  get email() { return this.registerForm.get('email');}
+  get password(){return this.registerForm.get('password');}
+  get username(){return this.registerForm.get('username');}
 }
