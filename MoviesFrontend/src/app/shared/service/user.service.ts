1c23424fcd9c6ece15721650038d59678886ebd0
Alejandro Aranda <alexarnd99@gmail.com>
2021-04-17 16:51:22 +0200

    Create interface to get token in user service

--- MoviesFrontend/src/app/shared/service/user.service.ts
+++ MoviesFrontend/src/app/shared/service/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AuthResult } from '../interfaces/auth-result.interface';
 import { NewUser } from '../interfaces/new-user.interface';
 import { UserLogin } from '../interfaces/user-login.interface';
 
@@ -15,7 +17,7 @@ export class UserService {
     return this.httpclient.post(environment.REST_API_SERVER+environment.REGISTER, newUser);
   }
 
-  public loginUser(user: UserLogin){
-    return this.httpclient.post(environment.REST_API_SERVER+environment.LOGIN, user)
+  public loginUser(user: UserLogin): Observable<AuthResult>{
+    return this.httpclient.post<AuthResult>(environment.REST_API_SERVER+environment.LOGIN, user);
   }
 }
